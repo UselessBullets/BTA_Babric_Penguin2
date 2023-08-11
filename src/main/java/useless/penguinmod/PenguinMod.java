acc9d3601f9802f06dcd7d5a8f5a0f22e2401bc4
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-11 08:33:41 -0500

    ?

--- src/main/java/useless/penguinmod/PenguinMod.java
+++ src/main/java/useless/penguinmod/PenguinMod.java
@@ -3,8 +3,10 @@ package useless.penguinmod;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.render.entity.CreeperRenderer;
 import net.minecraft.core.entity.monster.EntityCreeper;
+import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.HalpLibe;
 import turniplabs.halplibe.helper.EntityHelper;
 
 
@@ -14,6 +16,8 @@ public class PenguinMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        LOGGER.info("PenguinMod initialization started.");
+        //HalpLibe.LOGGER.info("test");
         EntityHelper.createEntity(EntityCreeper.class, new CreeperRenderer(), 200, "Penguin");
         LOGGER.info("PenguinMod initialized.");
     }
